c82e82a80dfb70b57c20f5bb99e9fadff8784a19
vonder2 <vonder@inbox.ru>
2020-02-23 01:05:26 +0200

    sync rewrite, no input existence check, added empty line error, have errors in input.txt

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-let inputFilePath = './input.txt';
+let inputFilePath = 'input.txt';
 if (process.argv[2]) {
-    // inputFilePath = './' + process.argv[2];
     inputFilePath = process.argv[2];
     console.log('Used filename provided as argument:', inputFilePath)
 }
@@ -36,74 +35,59 @@ if (process.argv[2]) {
 
 // ---------------
 
-const { once } = require('events');
-const { createReadStream, open } = require('fs');
+// const { once } = require('events');
+const { createReadStream } = require('fs');
 const { createInterface } = require('readline');
 const Mower = require('./Mower.js');
 
-open(inputFilePath, (err) => {
-    if (err) {
-        if (err.code === 'ENOENT') {
-            console.error(inputFilePath, 'does not exist!');
-            console.error('Create "input.txt" file at local directory or provide different file name as run argument');
-            // return;
-        }
-        throw err;
+//  require('fs').open(inputFilePath, (err) => {
+//     if (err) {
+//         if (err.code === 'ENOENT') {
+//             console.error(inputFilePath, 'does not exist!');
+//             console.error('Create "input.txt" file at local directory or provide different file name as run argument');
+//             // return;
+//         }
+//         throw err;
+//     }
+// });
+
+const lineReader = createInterface({ input: createReadStream(inputFilePath), crlfDelay: Infinity });
+
+let lineCounter = 1;
+let area;
+let mowerConfig = null;
+
+lineReader.on('line', (line) => {
+
+    // console.log(lineCounter + ')', line);
+    if (lineCounter === 1)
+    {
+        // get Area size
+        area = line.trim().split(' ').map(c => parseInt(c));
+
+        // error check
+        if (area.length !== 2) throw 'Wrong area dimension: ' + area.toString();
+        if (area[0] < 1 || area[1] < 1) throw new Error('Wrong area size: sides cannot be < 1');
     }
+    else if (lineCounter % 2 === 0)
+    {
+        // get current Mower
+        mowerConfig = line.trim().split(' ');
 
+        // error check
+        if (!line) console.error('Line', lineCounter, 'is empty! No initial position and orientation of the mower.');
+        if (mowerConfig.length !== 3)
+            throw new Error('Wrong mower config on line ' + lineCounter + ' of ' + inputFilePath);
+        // mowerPath = null;
+    }
+    else
+    {
+        // get current Mower path
+        const autoMower = new Mower(mowerConfig, area);
+        autoMower.go(line);
+        console.log(autoMower.position);
 
+        // console.log('Current mower', mowerConfig, 'path:', line);
+    }
+    lineCounter++;
 });
-
-(async function processLineByLine() {
-    try {
-        const lineReader = createInterface({ input: createReadStream(inputFilePath), crlfDelay: Infinity });
-
-        let lineCounter = 0
-        let area;
-        let currentMower = null;
-        // let mowerPath = null;
-
-        lineReader.on('line', (line) => {
-            // get Area size
-            if (lineCounter === 0)
-            {
-                area = line.trim().split(' ').map(c => parseInt(c));
-                if (area.length !== 2) throw 'Wrong area dimension: ' + area.toString();
-                if (area[0] < 1 || area[1] < 1) throw new Error('Wrong area size: sides cannot be < 1');
-            }
-            else if (lineCounter % 2)
-            { // get current Mower
-                currentMower = line.trim().split(' ');
-                if (currentMower.length !== 3) throw new Error('Wrong mower config on line ' + lineCounter);
-                // mowerPath = null;
-            }
-            else
-            { // get current Mower path
-                // mowerPath = line;
-                // console.log('Current mower', currentMower, 'path:', line);
-
-                const autoMower = new Mower(currentMower, area);
-                autoMower.go(line);
-                console.log(autoMower.position);
-            }
-
-            // console.log(lineCounter + ')', line);
-            lineCounter++;
-        });
-
-        // await once(lineReader, 'close');
-        // console.log('File processed:', lineCounter, 'lines');
-
-        // if (lineCounter % 2 === 0 || lineCounter < 3) throw new Error('Input has wrong format');
-
-
-    }
-    catch (err) {
-        console.error('!!ERROR!!')
-        console.error(err);
-    }
-})();
-
-
-
-
